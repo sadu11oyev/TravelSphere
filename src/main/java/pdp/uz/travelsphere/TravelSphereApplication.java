a322e68033d73354a308dc00bd12f78fa33de05e
Бахтиёр Садуллоев <baxtisadulloyev@gmail.com>
2024-09-12 11:27:21 +0500

    firstCommit

---
+++ src/main/java/pdp/uz/travelsphere/TravelSphereApplication.java
@@ -0,0 +1,13 @@
+package pdp.uz.travelsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelSphereApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pdp/uz/travelsphere/TravelSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package pdp.uz.travelsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
